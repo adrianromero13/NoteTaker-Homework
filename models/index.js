a944736987900ed6e18efa186fe078479f01e8e7
Adrian Romero <radrian031613@gmail.com>
2020-03-20 10:15:10 -0700

    created a models and a routes folder to modularize code. added module export code in the models folder.

---
+++ models/index.js
@@ -0,0 +1,4 @@
+module.exports = {
+    Home: require('./Home'),
+    Notes: require('./Notes')
+};
--- server.js
+++ server.js
@@ -19,4 +19,4 @@ app.get('/', (req, res) => {
 
 app.use(express.static('public'));
 
-app.listen(PORT);
+app.listen(PORT, () => console.log(`Server started on PORT ${PORT}`));
